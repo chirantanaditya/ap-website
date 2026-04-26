d751e881d78496cbfa7f226142ed5764ee0c1dc6
Aditya Chirantan Pande <chirantanaditya@gmail.com>
2026-04-27 00:53:44 +0530

    time change

--- src/data/schedule.ts
+++ src/data/schedule.ts
@@ -14,9 +14,9 @@ const SHARMAS_HOUSE_URL = 'https://www.google.com/maps?q=28.521366119384766,77.1
 
 export const BRIDE_SCHEDULE: ScheduleRow[] = [
   { date: '28 April', event: 'Sangeet', time: '7 PM Onwards', venue: 'Venue: Cherish Ballrooms', venueUrl: SANGEET_WEDDING_URL },
-  { date: '29 April', event: 'Haldi', time: '12 PM Onwards', venue: "Sharma's House, Mehrauli", venueUrl: SHARMAS_HOUSE_URL },
-  { date: '29 April', event: 'Mehendi', time: '6PM onwards.', venue: "Sharma's House, Mehrauli", venueUrl: SHARMAS_HOUSE_URL },
-  { date: '30 April', event: 'Jaimaal', time: '7PM', venue: 'Venue: Cherish Ballrooms', venueUrl: SANGEET_WEDDING_URL },
+  { date: '29 April', event: 'Haldi', time: '4 PM Onwards', venue: "Sharma's House, Mehrauli", venueUrl: SHARMAS_HOUSE_URL },
+  { date: '29 April', event: 'Mehendi', time: '6 PM onwards.', venue: "Sharma's House, Mehrauli", venueUrl: SHARMAS_HOUSE_URL },
+  { date: '30 April', event: 'Jaimaal', time: '7:04 PM', venue: 'Venue: Cherish Ballrooms', venueUrl: SANGEET_WEDDING_URL },
   { date: '30 April', event: 'Pheras', time: '8 PM', venue: 'Venue: Cherish Ballrooms', venueUrl: SANGEET_WEDDING_URL },
 ]
 
